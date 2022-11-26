7ae2e6a611332c2b42cf23907b924cb92aa72f6d
Jeremy Gilmore <103142661+OgreLord3@users.noreply.github.com>
2022-11-26 17:55:28 -0500

    Add files via upload

--- ChainBlockUI.py
+++ ChainBlockUI.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[15]:
+# In[59]:
 
 
 from tkinter import *
@@ -11,10 +11,35 @@ from ChainBlockMethods import blockUser, unblockUser, scanTweets
 import webbrowser
 from PIL import Image, ImageTk
 from pathlib import Path
+from functools import partial
 
 def call_back(event):
     webbrowser.open("https://github.com/jmale021/ChainBlock")
+
+def unblockWindow():
+    newWindow = Toplevel(root)
+    newWindow.title('Unblock Users')
+    icon_photo = Image.open("ChainBlock_logo_16.jpeg")
+    icon = ImageTk.PhotoImage(icon_photo)
+    newWindow.iconphoto(False, icon)
+    newWindow.geometry("300x300")
     
+    title = Label(newWindow, text = "Select a user to unblock.",  bg = "red", fg = "white")
+    title['font'] = font_size
+    title.pack(fill = X)
+    
+    #read in the list of blocked users to fill a dropdown menu
+    with open('blocked_users.txt', 'r') as blocked_users:
+        #strip newline characters from text file
+        temp = blocked_users.readlines()
+        targets = [x.strip('\n') for x in temp]
+        
+    dropdown = ttk.Combobox(newWindow, values = targets, state = 'readonly')
+    dropdown.pack(side = TOP, pady = "10")
+    
+    unblock_confirm = Button(newWindow, text = "Unblock", width = "20", compound = CENTER, fg = "red", command = partial(unblock_actions, dropdown))
+    unblock_confirm['font'] = font_size
+    unblock_confirm.pack(side = BOTTOM, pady = "10")
     
 #def get_auth_url():
     #global oauth1_user_handler
@@ -41,18 +66,28 @@ def block_actions():
             #block all targeted users, provide user feedback and write to list of blocked users
             for user in targets:
                 username = blockUser(user)
-                block_box.insert(INSERT, username + " has been blocked!\n")    
+                block_box.insert(INSERT, "@" + username + " has been blocked!\n")    
                 blocked_users.write(username + "\n")
                 #must also delete the blocked user from block_targets
                 with open('block_targets.txt', 'w+') as update_targets:
                     temp2 = update_targets.readlines()
                     for y in temp2:
                         if y.strip('\n') != user:
-                            block_targets.write(y + "\n")
+                            block_targets.write(y + '\n')
     
-def unblock_actions():
-    username = unblockUser()
-    block_box.insert(INSERT, username + " has been unblocked!\n")
+def unblock_actions(user):
+    #grab the selected user, unblock and provide feedback
+    target = user.get()
+    username = unblockUser(target)
+    block_box.insert(INSERT, "@" + username + " has been unblocked!\n")
+    
+    #must also delete the unblocked user from blocked_users  
+    with open('blocked_users.txt', 'r') as update_blocks:
+        temp = update_blocks.readlines()
+    with open('blocked_users.txt', 'w') as update_blocks:
+        for x in temp:
+            if x.strip('\n') != username:
+                update_blocks.write(x)
     
 def analytics_actions():
     path_to_file = 'blocked_users.txt'
@@ -68,11 +103,11 @@ def scan_actions():
     users = scanTweets()
     with open('block_targets.txt', 'a+') as block_targets:
         for name in users:
-            block_box.insert(INSERT, name + " is a block target!\n")
+            block_box.insert(INSERT, "@" + name + " is a block target!\n")
             block_targets.write(name + "\n")
 
 
-# In[16]:
+# In[60]:
 
 
 root = Tk()
@@ -107,21 +142,21 @@ title_text.bind("<Button-1>", call_back)
 #pin_button['font'] = font_size
 #pin_button.pack(side = TOP, pady = "5")
 
+scan_button = Button(frame, text = "Scan", width = "20", fg = "red", command = scan_actions)
+scan_button['font'] = font_size
+scan_button.pack(side = TOP, pady = "5")
+
 block_button = Button(frame, text = "Block People", width = "20", fg = "red", command = block_actions)
 block_button['font'] = font_size
 block_button.pack(side = TOP, pady = "5")
 
-unblock_button = Button(frame, text = "Unblock People", width = "20", fg = "red", command = unblock_actions)
+unblock_button = Button(frame, text = "Unblock People", width = "20", fg = "red", command = unblockWindow)
 unblock_button['font'] = font_size
 unblock_button.pack(side = TOP, pady = "5")
 
 analytics_button = Button(frame, text = "View Analytics", width = "20", fg = "red", command = analytics_actions)
 analytics_button['font'] = font_size
 analytics_button.pack(side = TOP, pady = "5")
-
-scan_button = Button(frame, text = "Scan", width = "20", fg = "red", command = scan_actions)
-scan_button['font'] = font_size
-scan_button.pack(side = TOP, pady = "5")
 
 block_box = Text(bottom_frame, fg = "red", width = "50", height = "10", borderwidth = "2", relief = "ridge")
 block_box['font'] = font_size
